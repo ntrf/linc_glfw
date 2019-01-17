63027dfad39f07088cf56b1b98211fe96b6756e2
Anton Nesterov <ntrf@znscode.net>
2019-01-17 17:24:21 +0300

    Ugly, but functional callback system

--- linc/linc_glfw.cpp
+++ linc/linc_glfw.cpp
@@ -1,15 +1,34 @@
-//hxcpp include should be first
 #include <hxcpp.h>
-#include "./linc_empty.h"
 
-namespace linc {
+#include "GLFW/glfw3.h"
 
-    namespace empty {
+namespace linc 
+{
+	namespace empty 
+	{
+		struct Listener
+		{
+			cpp::Function< void(int, int) > PosCb;
+			cpp::Function< void(int, int) > SizeCb;
+			cpp::Function< void() > CloseCb;	
+			cpp::Function< void() > RefreshCb;
+			cpp::Function< void(int) > FocusCb;
+			cpp::Function< void(int) > IconifyCb;
+			cpp::Function< void(int, int) > FramebufferSizeCb;
+			cpp::Function< void(int, int, int) > MouseButtonCb;
+			cpp::Function< void(double, double) > CursorPosCb;
+			cpp::Function< void(int) > CursorEnterCb;
+			cpp::Function< void(double, double) >  ScrollCb;
+			cpp::Function< void(int, int, int, int) > KeyCb;
+			cpp::Function< void(unsigned int) > CharCb;
+			cpp::Function< void(unsigned int, int) > CharModsCb;
+			//cpp::Function< void(int, const char **) > DropCb;  // This needs array of strings
+		};
 
-        int example() {
-            return 55;
-        }
 
-    } //empty namespace
+		void installWindowListener(GLFWwindow * w) {
 
-} //linc
+			//glfwCreateWindow
+		}
+	}
+}
