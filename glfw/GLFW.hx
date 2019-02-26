13df58b242986f2c694ed11f25f2aa485bc0c930
Anton Nesterov <ntrf@znscode.net>
2019-02-26 18:25:57 +0300

    Compatibility wirth Haxe 4 RC

--- glfw/GLFW.hx
+++ glfw/GLFW.hx
@@ -103,59 +103,59 @@ extern class GLFW
 
 
 	@:native("glfwInit")
-	public static function init() : Int {}
+	public static function init() : Int;
 
 	@:native("glfwTerminate")
-	public static function terminate() : Void {}
+	public static function terminate() : Void;
 	
 	@:native("glfwPollEvents")
-	public static function pollEvents() : Void {}
+	public static function pollEvents() : Void;
 
 	@:native("glfwWaitEvents")
-	public static function waitEvents() : Void {}
+	public static function waitEvents() : Void;
 
 	@:native("glfwWaitEventsTimeout")
-	public static function waitEventsTimeout(delay : cpp.Float64) : Void {}
+	public static function waitEventsTimeout(delay : cpp.Float64) : Void;
 
 	@:native("glfwPostEmptyEvent")
-	public static function postEmptyEvent() : Void {}
+	public static function postEmptyEvent() : Void;
 
 	@:native("glfwWindowHint")
-	static function hint(hint : Int, value : Int) : Void {}
+	static function hint(hint : Int, value : Int) : Void;
 
 	@:native("glfwDefaultWindowHints")
-	static function defaultHints() : Void {}
+	static function defaultHints() : Void;
 
 	@:native("glfwGetVersionString")
-	public static function getVersionString() : cpp.ConstCharStar {}
+	public static function getVersionString() : cpp.ConstCharStar;
 
 	@:native("glfwGetTime")
-	public static function getTime() : cpp.Float64 {}
+	public static function getTime() : cpp.Float64;
 
 	@:native("glfwSetTime")
-	public static function setTime(v : cpp.Float64) : Void {}
+	public static function setTime(v : cpp.Float64) : Void;
 
 	@:native("glfwGetTimerValue")
-	public static function getTimerValue() : cpp.UInt64 {}
+	public static function getTimerValue() : cpp.UInt64;
 	@:native("glfwGetTimerFrequency")
-	public static function getTimerFrequency() : cpp.UInt64 {}
+	public static function getTimerFrequency() : cpp.UInt64;
 
 	@:native("glfwGetCurrentContext")
-	public static function getCurrentContext() : Window {}
+	public static function getCurrentContext() : Window;
 
 	@:native("glfwSwapInterval")
-	public static function swapInterval(interval : Int) : Void {}
+	public static function swapInterval(interval : Int) : Void;
 
 	@:native("glfwExtensionSupported")
-	public static function isExtensionSupported(name : ConstCharStar) : Int {}
+	public static function isExtensionSupported(name : ConstCharStar) : Int;
 	//@:native("glfwGetProcAddress")
 
 	//### VK support needs some additional cpp-only code + SDK
 	//    not sure how i'm going to hande it
 	@:native("glfwVulkanSupported")
-	public static function isVulkanSupported() : Int {}
+	public static function isVulkanSupported() : Int;
 	@:native("glfwGetRequiredInstanceExtensions")
-	public static function getRequiredInstanceExtensions(count : cpp.RawPointer<cpp.UInt32>) : cpp.RawPointer<ConstCharStar> {}
+	public static function getRequiredInstanceExtensions(count : cpp.RawPointer<cpp.UInt32>) : cpp.RawPointer<ConstCharStar>;
 
 #if GLFW_INCLUDE_VULKAN
 	//@:native("glfwGetInstanceProcAddress")
@@ -163,19 +163,18 @@ extern class GLFW
 	public static function getPhysicalDevicePresentationSupport(
 		instance : GlfwVkInstance, 
 		device : GlfwVkPhysicalDevice,
-		queuefamily : cpp.UInt32) : Int {}
+		queuefamily : cpp.UInt32) : Int;
 	@:native("glfwCreateWindowSurface")
 	public static function createWindowSurface(
 		instance : GlfwVkInstance, 
 		window : Window, 
 		allocator : cpp.RawConstPointer< cpp.Void >,
-		surface : cpp.RawPointer< GlfwVkSurfaceKHR >) : GlfwVkResult {}; 
+		surface : cpp.RawPointer< GlfwVkSurfaceKHR >) : GlfwVkResult;
 #end
 
 	// Error interface
 	@:native("glfwSetErrorCallback")
-	static function _i_setErrorCallback(fn: cpp.Callable< (cpp.Int32, cpp.ConstCharStar) -> Void >) : Void
-	{}
+	static function _i_setErrorCallback(fn: cpp.Callable< (cpp.Int32, cpp.ConstCharStar) -> Void >) : Void;
 
 	public inline static function setErrorCallback(fn : ErrorCb) : Void
 	{
