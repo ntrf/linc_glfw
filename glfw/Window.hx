199f7cf9ea392863464c44d8e15bcc854b1dfe64
Anton Nesterov <ntrf.zns@gmail.com>
2019-02-26 18:25:57 +0300

    Compatibility wirth Haxe 4 RC

--- glfw/Window.hx
+++ glfw/Window.hx
@@ -33,45 +33,45 @@ typedef DropCb = (paths : Array< String >) -> Void;
 extern class Window
 {
 	@:native("glfwMakeContextCurrent")
-	static function _internal_makeCurrent(win : Window) : Void {}
+	static function _internal_makeCurrent(win : Window) : Void;
 
 	@:native("glfwWindowShouldClose")
-	static function _internal_shouldClose(win : Window) : Int { return false; }
+	static function _internal_shouldClose(win : Window) : Int;
 
 	@:native("glfwSetWindowShouldClose")
-	static function _internal_setShouldClose(win : Window, v : Int) : Void {}
+	static function _internal_setShouldClose(win : Window, v : Int) : Void;
 
 	@:native("glfwSwapBuffers")
-	static function _internal_swapBuffers(win : Window) : Void { }
+	static function _internal_swapBuffers(win : Window) : Void;
 
 	@:native("glfwDestroyWindow")
-	static function _i_destroy(win : Window) : Void { }
+	static function _i_destroy(win : Window) : Void;
 
 	@:native("glfwShowWindow")
-	static function _i_show(win : Window) : Void { }
+	static function _i_show(win : Window) : Void;
 	@:native("glfwHideWindow")
-	static function _i_hide(win : Window) : Void { }
+	static function _i_hide(win : Window) : Void;
 	@:native("glfwIconifyWindow")
-	static function _i_iconify(win : Window) : Void { }
+	static function _i_iconify(win : Window) : Void;
 	@:native("glfwRestoreWindow")
-	static function _i_restore(win : Window) : Void { }
+	static function _i_restore(win : Window) : Void;
 	@:native("glfwMaximizeWindow")
-	static function _i_maximize(win : Window) : Void { }
+	static function _i_maximize(win : Window) : Void;
 	@:native("glfwFocusWindow")
-	static function _i_focus(win : Window) : Void { }
+	static function _i_focus(win : Window) : Void;
 
 	@:native("glfwGetWindowSize")
-	static function _i_getSize(win : Window, width : RawPointer<Int>, height : RawPointer<Int>) : Void { }
+	static function _i_getSize(win : Window, width : RawPointer<Int>, height : RawPointer<Int>) : Void;
 	@:native("glfwSetWindowSize")
-	static function _i_setSize(win : Window, width : Int, height : Int) : Void { }
+	static function _i_setSize(win : Window, width : Int, height : Int) : Void;
 
 	@:native("glfwGetWindowPos")
-	static function _i_getPos(win : Window, xpos : RawPointer<Int>, ypos : RawPointer<Int>) : Void { }
+	static function _i_getPos(win : Window, xpos : RawPointer<Int>, ypos : RawPointer<Int>) : Void;
 	@:native("glfwSetWindowPos")
-	static function _i_setPos(win : Window, xpos : Int, ypos : Int) : Void { }
+	static function _i_setPos(win : Window, xpos : Int, ypos : Int) : Void;
 
 	@:native("glfwGetFramebufferSize")
-	static function _i_getFramebufferSize(win : Window, width : RawPointer<Int>, height : RawPointer<Int>) : Void { }
+	static function _i_getFramebufferSize(win : Window, width : RawPointer<Int>, height : RawPointer<Int>) : Void;
 
 	//@:native("glfwSetWindowTitle")
 	//@:native("glfwSetWindowIcon")
@@ -81,45 +81,45 @@ extern class Window
 	//@:native("glfwGetWindowMonitor")
 	//@:native("glfwSetWindowMonitor")
 	@:native("glfwGetWindowAttrib")
-	static function _i_getAttrib(win : Window, attr : Int) : Int { return 0; }
+	static function _i_getAttrib(win : Window, attr : Int) : Int;
 
 	//### I'm not sure how to abstract these
 	@:native("glfwGetWindowUserPointer")
-	static function _i_getUserPointer(win : Window) : RawPointer<cpp.Void> { return null; }
+	static function _i_getUserPointer(win : Window) : RawPointer<cpp.Void>;
 	@:native("glfwSetWindowUserPointer")
-	static function _i_setUserPointer(win : Window, ptr : RawPointer<cpp.Void>) : Void {}
+	static function _i_setUserPointer(win : Window, ptr : RawPointer<cpp.Void>) : Void;
 
 	//### ???
 	@:native("glfwSetWindowPosCallback")
-	static function _i_setCbWindowPos(win : Window, fn : cpp.Callable< (w:Window, x:cpp.Int32, y:cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbWindowPos(win : Window, fn : cpp.Callable< (w:Window, x:cpp.Int32, y:cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetWindowSizeCallback")
-	static function _i_setCbWindowSize(win : Window, fn : cpp.Callable< (w:Window, x:cpp.Int32, y:cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbWindowSize(win : Window, fn : cpp.Callable< (w:Window, x:cpp.Int32, y:cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetWindowCloseCallback")
-	static function _i_setCbWindowClose(win : Window, fn : cpp.Callable< (Window) -> Void >) : Void {}
+	static function _i_setCbWindowClose(win : Window, fn : cpp.Callable< (Window) -> Void >) : Void;
 	@:native("glfwSetWindowRefreshCallback")
-	static function _i_setCbWindowRefresh(win : Window, fn : cpp.Callable< (Window) -> Void >) : Void {}
+	static function _i_setCbWindowRefresh(win : Window, fn : cpp.Callable< (Window) -> Void >) : Void;
 	@:native("glfwSetWindowFocusCallback")
-	static function _i_setCbWindowFocus(win : Window, fn : cpp.Callable< (w : Window, f : cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbWindowFocus(win : Window, fn : cpp.Callable< (w : Window, f : cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetWindowIconifyCallback")
-	static function _i_setCbWindowIconify(win : Window, fn : cpp.Callable< (w : Window, a : cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbWindowIconify(win : Window, fn : cpp.Callable< (w : Window, a : cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetFramebufferSizeCallback")
-	static function _i_setCbFramebufferSize(win : Window, fn : cpp.Callable< (w: Window, a: cpp.Int32, b:cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbFramebufferSize(win : Window, fn : cpp.Callable< (w: Window, a: cpp.Int32, b:cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetMouseButtonCallback")
-	static function _i_setCbMouseButton(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32, b:cpp.Int32, c:cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbMouseButton(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32, b:cpp.Int32, c:cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetKeyCallback")
-	static function _i_setCbKey(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32, b:cpp.Int32, c:cpp.Int32, d:cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbKey(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32, b:cpp.Int32, c:cpp.Int32, d:cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetCharCallback")
-	static function _i_setCbChar(win : Window, fn : cpp.Callable< (w : Window, a: cpp.UInt32) -> Void >) : Void {}
+	static function _i_setCbChar(win : Window, fn : cpp.Callable< (w : Window, a: cpp.UInt32) -> Void >) : Void;
 	@:native("glfwSetCharModsCallback")
-	static function _i_setCbCharMods(win : Window, fn : cpp.Callable< (w : Window, a: cpp.UInt32, b:cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbCharMods(win : Window, fn : cpp.Callable< (w : Window, a: cpp.UInt32, b:cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetCursorPosCallback")
-	static function _i_setCbCursorPos(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Float64, b:cpp.Float64) -> Void >) : Void {}
+	static function _i_setCbCursorPos(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Float64, b:cpp.Float64) -> Void >) : Void;
 	@:native("glfwSetCursorEnterCallback")
-	static function _i_setCbCursorEnter(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32) -> Void >) : Void {}
+	static function _i_setCbCursorEnter(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32) -> Void >) : Void;
 	@:native("glfwSetScrollCallback")
-	static function _i_setCbScroll(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Float64, b:cpp.Float64) -> Void >) : Void {}
+	static function _i_setCbScroll(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Float64, b:cpp.Float64) -> Void >) : Void;
 	@:native("glfwSetDropCallback")
-	static function _i_setCbDrop(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32, b:cpp.RawPointer< cpp.ConstCharStar >) -> Void >) : Void {}
+	static function _i_setCbDrop(win : Window, fn : cpp.Callable< (w : Window, a: cpp.Int32, b:cpp.RawPointer< cpp.ConstCharStar >) -> Void >) : Void;
 
 
 	//@:native("glfwGetInputMode")
@@ -138,8 +138,7 @@ extern class Window
 	@:native("glfwCreateWindow")
 	@:hide
 	private static function _internal_createWindow(width : Int, height : Int, title : cpp.ConstCharStar, 
-		monitor : cpp.RawPointer<cpp.Void>, share : cpp.RawPointer<cpp.Void>) : Window
-	{ return null; }
+		monitor : cpp.RawPointer<cpp.Void>, share : cpp.RawPointer<cpp.Void>) : Window;
 
 	public inline static function createWindow(width : Int, height : Int, title : String) : Window
 	{
